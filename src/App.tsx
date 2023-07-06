0f1b601ffd0531bb6a45bfe6bd7f438654af9be3
haku-ohsumi <haku818181@gmail.com>
2023-07-07 00:43:14 +0900

    FormとResultsをつなげたよ

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,45 @@
+import { useState } from 'react';
 import Title from "./components/Title";
 import Form from "./components/Form";
 import Results from "./components/Results";
 import './App.css';
 
+type ResultsStateType = {
+  country: string;
+  cityName: string;
+  temperature: string;
+  conditionText: string;
+  icon: string;
+}
+
 function App() {
+  const [city, setCity] = useState<string>("");
+  const [results, setResults] = useState<ResultsStateType>({
+    country:"",
+    cityName:"",
+    temperature:"",
+    conditionText:"",
+    icon:""
+  });
+  const getWeather = (e: any) => {
+    e.preventDefault();
+    fetch("https://api.weatherapi.com/v1/current.json?key=8b1c6068fb114c59952141728230507&q=London&aqi=no")
+    .then(res => res.json())
+    .then(data => {
+      setResults({
+        country: data.location.country,
+        cityName: data.location.name,
+        temperature: data.current.temp_c,
+        conditionText: data.current.condition.text,
+        icon: data.current.condition.icon
+      })
+    })
+  }
   return (
     <div className="test">
       <Title/>
-      <Form/>
+      <Form setCity = {setCity} getWeather = {getWeather}/>
+      <Results results = {results}/>
     </div>
   );
 }
